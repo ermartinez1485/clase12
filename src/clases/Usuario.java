7aaeb29e360a6841e2c503459bebc58c33827e0b
Casa <Casa@Dell>
2015-10-14 18:15:30 -0600

    Merge origin/master

---
+++ src/clases/Usuario.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clases;
+
+/**
+ *
+ * @author Casa
+ */
+public class Usuario {
+    
+    private static String USER = "eric";
+    private static String PASS = "password";
+    
+    private String nombreUsuario;
+    private String contrasena;
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    
+    
+}
